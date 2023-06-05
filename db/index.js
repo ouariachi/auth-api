16ea8b506b01d8c82eadbe7d81d63c82a438c9a4
ouariachi <mmohamedelouariachi@gmail.com>
2023-06-05 02:19:26 +0200

    initiAPI as callback of initDB

--- db/index.js
+++ db/index.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 import LogManager from "../logs/index.js"
 
-const logManager = new LogManager();
-const db = mongoose.connection;
 
-//CONNECTION LISTENERS
-db.on("connected", () => {
-    logManager.dataBase(`[initDB] {${new Date().getTime()}} Connected to DataBase.`);
-    console.log(">> Conectado a base de datos.");
-});
-db.on("error", () => { 
-    logManager.dataBase(`[initDB] {${new Date().getTime()}} Data base error.`);
-    console.error(">> No se pudo conectar a la base de datos."); 
-});
-
-export function initDB(){
+export function initDB(callback){
+    const logManager = new LogManager();
+    
     // eslint-disable-next-line no-undef
     mongoose.connect(process.env.DB_URL).catch(err => console.log(err));
+    
+    //CONNECTION LISTENERS
+    const db = mongoose.connection;
+    
+    db.on("connected", () => {
+        logManager.dataBase(`[initDB] {${new Date().getTime()}} Connected to DataBase.`);
+        console.log(">> Conectado a base de datos.");
+        
+        if(callback) callback();
+    });
+    
+    db.on("error", () => { 
+        logManager.dataBase(`[initDB] {${new Date().getTime()}} Data base error.`);
+        console.error(">> No se pudo conectar a la base de datos."); 
+    });
 }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { initAPI } from "./api/index.js";
 import { initDB } from "./db/index.js";
 
-initDB();
-initAPI();
+initDB(initAPI());
